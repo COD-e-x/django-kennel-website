63f30ac4dd2e154b0b9205465ad76240660f57c5
Shamil Lachenilaev <lachen1305@gmail.com>
2025-03-25 15:53:10 +0300

    Added a custom model "User" and set up in admin.py

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,65 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from apps.users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "phone",
+        "telegram",
+        "is_active",
+        "date_joined",
+        "last_login",
+    )
+    list_display_links = ("id", "email")
+    list_editable = (
+        "is_active",
+        "phone",
+        "telegram",
+    )
+    search_fields = ("email", "phone", "telegram")
+    list_filter = (
+        "is_active",
+        "date_joined",
+        "last_login",
+    )
+    readonly_fields = ("date_joined", "last_login")
+    ordering = ("id",)
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "phone",
+                    "telegram",
+                    "first_name",
+                    "last_name",
+                    "is_active",
+                )
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": ("is_staff", "is_superuser", "groups", "user_permissions"),
+            },
+        ),
+        (
+            _("Important dates"),
+            {
+                "fields": ("last_login", "date_joined"),
+            },
+        ),
+        # (
+        #     _("Password"),
+        #     {
+        #         "fields": ("password",),
+        #     },
+        # ),
+    )
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,86 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+NULLABLE = {"null": True, "blank": True}
+NULLABLE_FOR_STRING = {"null": False, "blank": True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name=_("Эл. почта"))
+    phone = models.CharField(
+        max_length=35,
+        verbose_name=_("Номер телефона"),
+        **NULLABLE_FOR_STRING,
+    )
+    telegram = models.CharField(
+        max_length=150,
+        verbose_name=_("Телеграм"),
+        **NULLABLE_FOR_STRING,
+    )
+    birth_date = models.DateField(
+        **NULLABLE,
+        verbose_name=_("Дата рождения"),
+    )
+    address = models.CharField(
+        max_length=255,
+        **NULLABLE_FOR_STRING,
+        verbose_name=_("Адрес"),
+    )
+    gender = models.CharField(
+        max_length=1,
+        choices=[
+            ("М", _("Мужской")),
+            ("Ж", _("Женский")),
+        ],
+        verbose_name=_("Пол"),
+    )
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/",
+        **NULLABLE,
+        verbose_name=_("Фото профиля"),
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=[
+            ("active", _("Активный")),
+            ("inactive", _("Неактивный")),
+            ("banned", _("Заблокирован")),
+        ],
+        default="active",
+        verbose_name=_("Статус"),
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name=_("Активен"),
+    )
+
+    # phone_verified = models.BooleanField(default=False, verbose_name=_("Телефон подтверждён"))
+    # telegram_verified = models.BooleanField(default=False, verbose_name=_("Telegram подтверждён"))
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _("Пользователь")
+        verbose_name_plural = _("Пользователи")
+        ordering = ["id"]
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
+        return self.first_name
+
+    def activate(self):
+        self.is_active = True
+        self.save()
+
+    def deactivate(self):
+        self.is_active = False
+        self.save()
