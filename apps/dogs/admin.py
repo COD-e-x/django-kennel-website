a87d95948016c342f02c030c0d91884009728c5b
Shamil Lachenilaev <lachen1305@gmail.com>
2025-03-25 16:58:26 +0300

    Register Breed and Dog in the admin

--- apps/dogs/admin.py
+++ apps/dogs/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from apps.dogs.models import Breed, Dog
 
-# Register your models here.
+
+@admin.register(Breed)
+class BreadAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "description",
+    )
+    ordering = ("id",)
+    search_fields = ("name",)
+
+
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "breed",
+        "birth_date",
+        "photo",
+    )
+    list_filter = (
+        "breed",
+        "birth_date",
+    )
+    ordering = ("name",)
+    search_fields = ("name",)
+    readonly_fields = ("photo",)
