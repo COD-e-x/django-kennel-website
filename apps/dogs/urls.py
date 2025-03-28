a8c857732aff5726dc2245dd9ede28b30943b307
Shamil Lachenilaev <lachen1305@gmail.com>
2025-03-28 16:30:50 +0300

    Setting up URLs routes for dogs app

--- apps/dogs/urls.py
+++ apps/dogs/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path("breeds/<int:pk>/dogs/", views.dogs_by_breed, name="dogs_by_breed"),
     path("dogs/", views.dogs_list, name="dogs_list"),
     path("dogs/create/", views.dog_create, name="dog_create"),
+    path("dogs/create/<int:pk>/", views.dog_edit, name="dog_edit"),
+    path("dogs/detail/<int:pk>/", views.dog_detail, name="dog_detail"),
+    path("dogs/update/<int:pk>/", views.dog_update, name="dog_update"),
+    path("dogs/delete/<int:pk>/", views.dog_delete, name="dog_create"),
+    path("dogs/delete/<int:pk>/", views.dog_delete, name="dog_delete"),
 ]
