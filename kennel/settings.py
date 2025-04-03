f608755af3e67471fae3b587743506f79afc1914
Shamil Lachenilaev <lachen1305@gmail.com>
2025-04-03 07:00:10 +0300

    Update static path

--- kennel/settings.py
+++ kennel/settings.py
@@ -124,15 +124,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = "static"
+STATIC_URL = "static/"
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = "staticfiles"
 
 MEDIA_URL = "/media/"
+
 MEDIA_ROOT = "media"
 
 # STATIC_URL = "static/"
-# STATICFILES_DIRS = (BASE_DIR / "static",)
-
 # MEDIA_ROOT = "media"
 # MEDIA_ROOT = BASE_DIR / "media"
 
